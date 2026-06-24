369a1569c639013f393e2a90d673608ba0ac5549
Lachlan Donald <lachlan+codex@buildkite.com>
2026-06-24 13:01:53 +1000

    chore: Prepare v1.1.0 release

--- src/root.zig
+++ src/root.zig
@@ -63,7 +63,7 @@ pub const virtio = struct {
     pub const vsock = @import("virtio/vsock.zig");
 };
 
-pub const version = "1.0.0";
+pub const version = "1.1.0";
 
 test {
     // Ensure all referenced modules' tests are discovered.
